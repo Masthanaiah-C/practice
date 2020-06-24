cb2baabc09551bd76bef2df37569b7ba49152811
mast <ctmasthanaiah5757@gmail.com>
2020-06-24 18:44:41 +0530

    added challenge

---
+++ delete.c
@@ -0,0 +1,30 @@
+//task challenge
+
+#include <stdio.h>
+
+int main()
+{int tm;
+scanf("%d",&tm);
+ int ser[tm];
+    for (int i=0;i<tm;i++)
+    {scanf("%d",&ser[i]);
+
+    }
+int x;
+scanf("%d",&x);int run=-1;
+for(int i=0;i<tm-1;i++)
+    {run+=x;
+    while(run>tm-1)
+        {
+            run=run-tm+i-1;
+        }
+        for(int k=run;k<tm;k++)
+    {
+    ser[k]=ser[k+1];
+    }
+    ser[tm-i-1]=0;
+    run--;
+    }
+printf("%d",ser[0]);
+return 0;
+}

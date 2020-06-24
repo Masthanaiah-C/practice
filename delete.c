a3de2f9240b4a5886da78f1c7354d2fa5c220264
mast <ctmasthanaiah5757@gmail.com>
2020-06-24 21:13:28 +0530

    sucess

--- delete.c
+++ delete.c
@@ -7,24 +7,26 @@ int main()
 scanf("%d",&tm);
  int ser[tm];
     for (int i=0;i<tm;i++)
-    {scanf("%d",&ser[i]);
+    {ser[i]=i+1;
 
     }
 int x;
 scanf("%d",&x);int run=-1;
 for(int i=0;i<tm-1;i++)
     {run+=x;
-    while(run>tm-i-1)
+    while(run>=(tm-i))
         {
-            run=run-tm-i-1;
+            run=run-tm+i;
         }
-        for(int k=run;k<tm-i;k++)
+        for(int k=run;k<(tm-i);k++)
     {
     ser[k]=ser[k+1];
     }
     ser[tm-i-1]=0;
-    run--;
-    }
+  
+    for(int j=0;j<tm;j++)
+    {printf("%d  ",ser[j]);}printf(" run =%d %d\n",run,i);run--;
+    }  
 printf("%d",ser[0]);
 
 
